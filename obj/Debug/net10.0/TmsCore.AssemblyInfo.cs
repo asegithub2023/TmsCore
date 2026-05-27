2c46c5fe51e5d0c73ed5ae8c6b6674ec507d7d16
Asefa Negash <aseaau21@gmail.com>
2026-05-27 09:16:41 +0300

    feat(module-1/session-1): implement null safety examples

--- obj/Debug/net10.0/TmsCore.AssemblyInfo.cs
+++ obj/Debug/net10.0/TmsCore.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TmsCore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce2a210269f34d7be8652e5e81a02e317225d4f9")]
 [assembly: System.Reflection.AssemblyProductAttribute("TmsCore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TmsCore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
